7c0708d90c3002070bbd8b641d755539b38e422b
Gaganjot singh <gaganjot25@gmail.com>
2022-08-07 17:16:58 +0530

    day 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti'
 
 function App() {
-  let [dice, setdice] = useState(olddice => olddice.map(die => {
-    return die.isHeld ? die : {
-      {value: Math.ceil(Math.random()*6),
-      isHeld: true,
-      id: nanoid()
-    }}
-  }))
+  let [dice, setdice] = useState(allNewDice())
+  let [tenzies, settenzies] = useState(false)
   function allNewDice() {
    const newDice = []
    for (let i = 0; i < 10; i++) {
     newDice.push({value: Math.ceil(Math.random()*6),
-    isHeld: true,
+    isHeld: false,
     id: nanoid()
   })
    }
@@ -28,17 +24,41 @@ function App() {
          }))
   }
 
-  const diceRoll = dice.map((die) => <Main key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
-  function diceRolling() {
-    setdice(allNewDice())
+  const diceRoll = dice.map((die) => <Main value={die.value} isHeld={die.isHeld} id={die.id} holdDice={() => holdDice(die.id)}/>)
+  function diceRolling() { 
+    if (!tenzies) {
+      
+         setdice(olddice => olddice.map(die => {
+      return (die.isHeld ? die : {
+         value: Math.ceil(Math.random()*6),
+         isHeld: false,
+         id: nanoid()
+      }
+     ) }))}
+     else {
+      settenzies(false)
+      setdice(allNewDice())
+     }
   }
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstHeld = dice[0].value
+    const sameValue = dice.every(die => die.value === firstHeld)
+    if (allHeld && sameValue) {
+      settenzies(true) }
+    
+  }, [dice])
   return (
     <div className="App">
       <div className="app2">
       <div className="dice">
-      {diceRoll}
-      <button className="button" onClick={diceRolling}>Roll the dice</button>
-    </div></div> </div>
+{ tenzies &&  <Confetti />}
+      {diceRoll} 
+      
+      
+   
+     <div className='button11'><button className="button" onClick={() => diceRolling()}>{ tenzies ? "Reset New Game" : "Roll the Dice" }</button></div>
+     </div></div></div>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,13 +4,16 @@
 
 
 export default function Main(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
+    }
 
     return (
-        <div className="main" onClick={props.holdDice}>
+        <div className="main" onClick={props.holdDice}y>
             
                 <div className="rando">
                     
-                    <div className={props.isHeld ? "dicecol" : "dicegreen"} >{props.value}</div>
+                    <div className="dicecol" style={styles}>{props.value}</div>
                     
                
                  
